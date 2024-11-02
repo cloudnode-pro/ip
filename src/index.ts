291577146b36cd469583eb316eac861de03c3b54
Zefir Kirilov <contact@zefir.pro>
2024-11-02 20:43:49 +0200

    copyright & licence notice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2024 Cloudnode OÜ
+ *
+ * This file is part of @cldn/ip.
+ *
+ * @cldn/ip is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * @cldn/ip is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with @cldn/ip.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 export {IPAddress} from "./IPAddress.js";
 export {IPv4} from "./IPv4.js";
 export {IPv6} from "./IPv6.js";
